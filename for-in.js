47f62603c4ec29dd71ea1be7f283a28b037212a2
Miqueias Rodrigues da Costa <miqsoftware1@hotmail.com>
2020-12-30 21:15:34 -0300

    Criado e configurado o arquivo for-in.js

---
+++ for-in.js
@@ -0,0 +1,9 @@
+//criação da array 
+const Languanges = ['JavaScript', 'Python', 'Visual Basic 6'];
+
+//forma como 'for in' funciona
+
+for(const i in Languanges ){
+    console.log(Languanges[i]);
+}
+
